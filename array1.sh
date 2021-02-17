e75bc8bdb423cb1c31c630721b9990b0a143637c
shashank <shashank.w1998@gmail.com>
2021-02-18 03:07:51 +0530

    addzer added

--- array1.sh
+++ array1.sh
@@ -71,3 +71,29 @@ done
 echo "Prime factors:" ${arr[@]}
 
 
+#!/bin/bash
+sum=0
+read –p “enter the array size” size
+echo “enter  the array element ”
+for ((i=1;i<=size;i++))
+read element
+arr[i]=$element
+done
+for ((i=o;i<=size;i++))
+do
+	first=${arr[i]}
+	for ((j=i+1;j<=size;j++))
+	do
+		second=${arr[j]}
+		for ((k=j+1;j<=size;j++))
+		do
+			third=${arr[k]}
+		sum=$((first+second+third))
+		done
+		if [ $sum –eq 0 ]
+		then 
+			echo “  $first $second $third ”
+		fi
+	done
+done
+
