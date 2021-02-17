1bf3cc1a7f642c06b4f1d2acd45566eed0e5700c
shashank <shashank.w1998@gmail.com>
2021-02-18 02:58:23 +0530

    array added

--- array1.sh
+++ array1.sh
@@ -34,3 +34,25 @@ done
 echo "second largest:" $second_max
 echo "second minimum;" $second_min
 
+
+#!/bin/bash
+for (( i=1;i<=10;i++))
+do
+   randomNumber=$((100+$RANDOM%900))
+   arr[$i]=$randomNumber
+done
+for ((i=1;i<=10;i++))
+do
+   for ((j=i+1;j<=10;j++))
+   do
+      if [ ${arr[$i]} -gt ${arr[$j]} ]
+      then
+         temp=${arr[$i]}
+         arr[$i]=${arr[j]}
+         arr[$j]=$temp
+      fi
+   done
+done
+echo "second max:" ${arr[9]}
+echo "second min:" ${arr[2]}
+
