149e689a5c831375b8fe3439aa698f2e639d61b5
shashank <shashank.w1998@gmail.com>
2021-02-18 03:13:28 +0530

    ran added

--- array1.sh
+++ array1.sh
@@ -97,3 +97,16 @@ do
 	done
 done
 
+#!/bin/bash
+count=1
+for ((i=1;i<=100;i++))
+do
+        temp=$(($i%11))
+        if [ $temp -eq 0 ]
+        then
+                arr[$count]=$i
+                count=$(($count+1))
+        fi
+done
+echo ${arr[@]}   
+
